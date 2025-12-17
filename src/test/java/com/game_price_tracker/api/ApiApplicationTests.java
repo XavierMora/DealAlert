a956b196ef2afe2c2c2208d523d003283eefa3c9
XavierMora <xaviermoravega11@gmail.com>
2025-12-17 20:03:41 -0300

    Initial commit

---
+++ src/test/java/com/game_price_tracker/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_price_tracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
