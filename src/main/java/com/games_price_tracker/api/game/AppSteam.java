ebe0696f0a16c37eba997125c9ab7dcfed6f7f96
XavierMora <xaviermoravega11@gmail.com>
2025-12-21 12:49:28 -0300

    Agrego AppSteam para deserializar steam applist

---
+++ src/main/java/com/games_price_tracker/api/game/AppSteam.java
@@ -0,0 +1,32 @@
+package com.games_price_tracker.api.game;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AppSteam{
+    @JsonProperty("appid")
+    private Long steamId;
+    private String name;
+
+    AppSteam(){}
+
+    public Long getSteamId() {
+        return steamId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSteamId(Long steamId) {
+        this.steamId = steamId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[steamId=%d, name=%s]", steamId, name);
+    }
+}
