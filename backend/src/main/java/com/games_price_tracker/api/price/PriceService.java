a83431cddb45f46e695e4c16961c0a529b495d47
XavierMora <xaviermoravega11@gmail.com>
2025-12-27 23:19:52 -0300

    Métodos crear y obtener precio

--- backend/src/main/java/com/games_price_tracker/api/price/PriceService.java
+++ backend/src/main/java/com/games_price_tracker/api/price/PriceService.java
@@ -1,7 +1,15 @@
 package com.games_price_tracker.api.price;
 
+import java.time.Instant;
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import com.games_price_tracker.api.game.Game;
+
 import jakarta.transaction.Transactional;
 
+@Service
 public class PriceService {
     private final PriceRepository priceRepository;
 
@@ -11,5 +19,14 @@ public class PriceService {
 
     boolean pricesChanged(Price oldPrice, int newInitialPrice, int newFinalPrice){
         return oldPrice.getInitialPrice() != newInitialPrice && oldPrice.getFinalPrice() != newFinalPrice;
+    }
+
+    @Transactional
+    Price createPrice(int initialPrice, int finalPrice, Game game){
+        return priceRepository.save(new Price(initialPrice, finalPrice, game));
+    }
+
+    Price getPrice(Long id) throws NoSuchElementException{
+        return priceRepository.findById(id).orElseThrow();
     }
 }
