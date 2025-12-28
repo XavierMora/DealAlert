2afbbbd49d6a6904a6cd884fb2a9cd08b163b3c9
XavierMora <xaviermoravega11@gmail.com>
2025-12-28 17:39:53 -0300

    Endpoint para obtener la información de un Game

--- backend/src/main/java/com/games_price_tracker/api/game/GameController.java
+++ backend/src/main/java/com/games_price_tracker/api/game/GameController.java
@@ -2,22 +2,30 @@ package com.games_price_tracker.api.game;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.games_price_tracker.api.game.dtos.GameInfo;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
     private final GameService gameService;
-
-    GameController(GameService gameService){
-        this.gameService = gameService;
-    }
-
-    @GetMapping
-    public String getGames(@RequestParam(required = false) String title) {
-        
-        return new String();
-    }
+    private final GameMapper gameMapper;
     
+    GameController(GameService gameService, GameMapper gameMapper){
+        this.gameService = gameService;
+        this.gameMapper = gameMapper;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameInfo> getGameInfo(@PathVariable(required = true) Long id) throws NoSuchElementException {
+        Game game = gameService.getGameById(id);
+
+        return ResponseEntity.ok(gameMapper.toGameInfo(game));
+    }    
 }
