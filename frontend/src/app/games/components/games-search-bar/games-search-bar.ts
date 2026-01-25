b7f7a3bcac1a86dbb4fe5cb7187d0ef2a212c61c
XavierMora <xaviermoravega11@gmail.com>
2026-01-25 11:43:15 -0300

    Estilos barra de búsqueda

--- frontend/src/app/games/components/games-search-bar/games-search-bar.ts
+++ frontend/src/app/games/components/games-search-bar/games-search-bar.ts
@@ -14,9 +14,9 @@ export class GamesSearchBar {
   emitNewName(value: string){
     if(this.lastEvent !== null) clearTimeout(this.lastEvent);
     
-    // Se notifica sobre el nombre que se busca si pasaron 500ms sin un nuevo evento
+    // Se notifica sobre el nombre que se busca si pasaron 400ms sin un nuevo evento
     this.lastEvent = setTimeout(() => {
       this.newName.emit(value);
-    }, 500); 
+    }, 400); 
   }
 }
